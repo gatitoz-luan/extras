29fffd67d93c18ba080fe58b77ded836d67c050b
Luan Costa <luansilvacosta2010@hotmail.com>
2021-11-20 23:40:37 -0300

    6

--- sites/script.js
+++ sites/script.js
@@ -1,5 +1,16 @@
 function calculatetip(event) {
-    alert('fui submetido')
+    event.preventDefault();
+    let ex = document.getElementById('exer').value;
+    let pe = document.getElementById('peso').value;
+    let rep = document.getElementById('falha').value;
+    let c=(1-(0.02*int(rep)));
+    let rm=(rm = int(pe)/c);
+    document.getElementById('tot').innerHTML = rm;
 }
-document.getElementById('totaltip').style.display="none";
-document.getElementById('tipsForm').addEventListener('submit',calculatetip)
+
+document.getElementById('totaltip').style.display="block";
+document.getElementById('tipsForm').addEventListener('submit',calculatetip);
+
+
+
+//// indicar: https://www.basefitness.com.br/?s=supino  ////
